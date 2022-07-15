170395b7273b059bd68395b8ab20dfddc590d70f
Thomas Sankara <thomassankara74@gmail.com>
2022-07-15 15:52:33 +0300

    add exclamation error when input is invalid

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ function validate(e) {
 
   if (!isEmail(inputValue)) {
     error.style.display = "block"
+    label.style.display = "block"
   } else {
     error.style.display = "none"
+    label.style.display = "none"
   }
 }
 
